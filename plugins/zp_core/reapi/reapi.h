b4b4fcde3b4833e7d45197bac6138bac4c7bff3f
Roony11-1 <eltheasasin@gmail.com>
2026-06-27 18:19:26 -0400

    Add WeaponInfoStruct to IReGameApi for enhanced weapon management

--- plugins/zp_core/reapi/reapi.h
+++ plugins/zp_core/reapi/reapi.h
@@ -10,6 +10,21 @@ struct ReGameFuncs_t
     edict_t *(*CREATE_NAMED_ENTITY2)(string_t iClass);
 };
 
+struct WeaponInfoStruct 
+{
+    int iSlot;
+    int iPosition;
+    const char *pszAmmo1;
+    int iMaxAmmo1;
+    const char *pszAmmo2;
+    int iMaxAmmo2;
+    const char *pszName;
+    int iMaxClip;
+    int iId;
+    int iFlags;
+    int iWeight;
+};
+
 class IReGameApi 
 {
     public:
@@ -17,6 +32,9 @@ class IReGameApi
         virtual int GetMajorVersion() = 0;
         virtual int GetMinorVersion() = 0;
         virtual const ReGameFuncs_t *GetFuncs() = 0;
+
+        virtual WeaponInfoStruct *GetWeaponInfo(int weaponID) = 0;
+        virtual WeaponInfoStruct *GetWeaponInfo(const char *weaponName) = 0;
 };
 
 extern IReGameApi *g_pReGameApi;
